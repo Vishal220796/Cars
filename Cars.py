036f31c7f04523d65ff63dd72512141ec8cbd5e6
NandurkarNeil <87815475+Vishal220796@users.noreply.github.com>
2023-05-10 18:29:24 +0530

    Add files via upload

---
+++ Cars.py
@@ -0,0 +1,45 @@
+class Vehicles:
+    
+    
+    
+    Car_name1 = "Tesla Model 3"
+    Car_name2 = "Mustang"
+    Car_name3 = "Honda"
+    Car_name4 = "Toyota"
+    Car_name5 = "Porsche"
+    Car_name6 = "Mahindra"
+    Car_name7 = "Hyundai"
+    Car_name8 = "Kia"
+    Car_name9 = "Ferrari"
+    Car_name10 = "Mclaren"
+    
+    
+    def main(self):
+        print("I have", self.Car_name1)
+        print("I have", self.Car_name2)
+        print("I have", self.Car_name3)  
+        print("I have", self.Car_name4)
+        print("I have", self.Car_name5)
+        print("I have", self.Car_name6)
+        print("I have", self.Car_name7)
+        print("I have", self.Car_name8)
+        print("I have", self.Car_name9)
+        print("I have", self.Car_name10)
+        
+        
+        
+        Vehicle_class_object = Vehicles()
+        
+        
+        
+        print("Your car is:", Vehicle_class_object.Car_name1)
+        print("Your car is:", Vehicle_class_object.Car_name2)
+        print("Your car is:", Vehicle_class_object.Car_name3)
+        print("Your car is:", Vehicle_class_object.Car_name4)
+        print("Your car is:", Vehicle_class_object.Car_name5)
+        print("Your car is:", Vehicle_class_object.Car_name6)
+        print("Your car is:", Vehicle_class_object.Car_name7)
+        print("Your car is:", Vehicle_class_object.Car_name8)
+        print("Your car is:", Vehicle_class_object.Car_name9)
+        print("Your car is:", Vehicle_class_object.Car_name10)
+        Vehicle_class_object.main()
